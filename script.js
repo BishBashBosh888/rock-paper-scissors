8b5a80c0ef71514f857813e7e87de064e159cb4e
fsut0005 <fsut0005@student.monash.edu>
2024-12-19 00:27:18 +1100

    added scoreboard to track human's and computer's score

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const buttons = document.querySelectorAll('.choices button');
 const result = document.getElementById('result');
+const scoreText = document.getElementById('score');
+let humanScore = 0;
+let computerScore = 0;
 
 const choices = ['Rock', 'Paper', 'Scissors'];
 
@@ -16,14 +19,20 @@ function getWinner(userChoice, computerChoice){
         (userChoice === 'Paper' && computerChoice === 'Rock') ||
         (userChoice === 'Scissors' && computerChoice === 'Paper')
     ) {
+        humanScore++;
         return "You win!";
     } else {
+        computerScore++;
         return "You lose!";
     }
 }
 
 function updateResult(userChoice, computerChoice, gameResult){
     result.textContent = `You chose ${userChoice}, Computer chose ${computerChoice}. ${gameResult}`;
+}
+
+function updateScoreText(humanScore,computerScore){
+    scoreText.textContent = `${humanScore} Human - Computer ${computerScore}`
 }
 
 buttons.forEach(button => {
@@ -33,6 +42,7 @@ buttons.forEach(button => {
         const computerChoice = getComputerChoice();
         console.log(computerChoice);
         const gameResult = getWinner(userChoice, computerChoice);
+        updateScoreText(humanScore,computerScore);
         updateResult(userChoice, computerChoice, gameResult)
     })
 });
